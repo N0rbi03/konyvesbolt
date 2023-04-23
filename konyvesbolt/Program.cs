3371b33e139a40853f6c5dcef6e4f55bc7f38530
N0rbi03 <tothnorbi2003@gmail.com>
2023-04-23 17:35:03 +0200

    Add project files.

---
+++ konyvesbolt/Program.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace konyvesbolt
+{
+    internal class Program
+    {
+        static MySqlConnection conn=null;
+        static MySqlCommand sql = null;
+
+        static void Main(string[] args)
+        {
+            MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder();
+            mySqlConnectionStringBuilder.Server = "localhost";
+            mySqlConnectionStringBuilder.Database = "konyvesbolt";
+            mySqlConnectionStringBuilder.UserID = "root";
+            mySqlConnectionStringBuilder.Password = "";
+            conn = new MySqlConnection(mySqlConnectionStringBuilder.ToString());
+            conn.Open();
+        }
+        static void legidosebb()
+        { 
+            //sql command lefuttatását,hozzáadását nem tudom ezeket is tanárúr git jéről szedtem mert mindent elfelejtettem kb
+        }
+    }
+}
---
+++ konyvesbolt/konyvesbolt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace konyvesbolt
+{
+    internal class konyvesbolt
+    {
+        static void mysql_connection()
+            {
+              
+            }
+
+    }
+}
